71229990ba83470ea27e8408d2bdfe13c2c67288
LucyMHall <lucymhall1995@gmail.com>
2019-07-01 15:47:22 +0100

    add booking model into data mapper setup

--- db/data_mapper_setup.rb
+++ db/data_mapper_setup.rb
@@ -4,12 +4,13 @@ require 'dm-postgres-adapter'
 # this app name will be the prefix to the database
 APP_NAME = 'makers_bnb'
 
+require_relative '/lib/booking.rb'
 #todo - add model files here
 
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/#{APP_NAME}_#{ENV['RACK_ENV']}")
 
-#This checks the models for validity and initializes all properties associated with relationships. 
+#This checks the models for validity and initializes all properties associated with relationships.
 DataMapper.finalize
 
 #create tables if they don't exist
