d02e9e1ad532bc76ede1b350a70248ed5455fc0a
petraartep <petra.pogacsas@gmail.com>
2019-07-02 11:08:03 +0100

    feature test in progress

--- app/app.rb
+++ app/app.rb
@@ -13,6 +13,30 @@ class MakersBnB < Sinatra::Base
     erb :index
   end
 
+  get  '/user/:id/space/new' do
+    @user_id = :id
+    erb :"/space/new"
+  end
+
+  post '/user/:id/space/new' do
+    user = User.get(params[:id])
+    user.space.create(name: params[:name],
+                      description: params[:description],
+                      price: params[:price],
+                      available_from: params[:available_from],
+                      available_to: params[:available_to])
+    redirect "/user/#{user.id}/space"
+  end
+
+
+  get 'user/:id/space/' do
+    @user = User.get(params[:id])
+    erb :"/space/user_space"    
+  end
+
+
+
+
 
   run! if app_file == $0
 end
--- spec/features/create_space_feature_spec.rb
+++ spec/features/create_space_feature_spec.rb
@@ -6,8 +6,19 @@
 feature 'create a space' do
   scenario 'I can create a space so that it can be rented' do
 
-    visit '/'
+    visit '/user/1/space/new'
+    fill_in('name', with: "Alexis' house")
+    fill_in('description', with: "Lovely house with good shower")
+    fill_in('price', with: "100")
+    fill_in('available_from', with: "02/07/2019")
+    fill_in('available_to', with: "02/09/2019")
+    click_button('Submit')
 
+    expect(page).to have_content("Alexis' house")
+    expect(page).to have_content("Lovely house with good shower")
+    expect(page).to have_content("100")
+    expect(page).to have_content("02/07/2019")
+    expect(page).to have_content("02/09/2019")
 
   end
 end
