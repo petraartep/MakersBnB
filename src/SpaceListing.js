cd7aa52250c734d3746719ddab16c67da1d4c82a
Alexis Josephides <ajosephides@gmail.com>
2019-07-03 12:44:59 +0100

    added js files for space listing. errors.

--- src/SpaceListing.js
+++ src/SpaceListing.js
@@ -1,5 +1,16 @@
 'use strict';
 
+$(document).ready(function() {
+
+  function getSpaces(){
+    var url = '/api/space';
+    $.get(url, function(data) {
+      console.log(data)
+    });
+  };
+});
+
+
 function SpaceListing() {
 
   
